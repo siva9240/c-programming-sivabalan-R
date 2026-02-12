66ef3baf87456cacc11ff5cff0cdfa6691d3eeeb
siva9240 <sivabalan9240@mountzion.ac.in>
2026-02-12 16:00:51 +0530

    Create largest of two numbers.c

---
+++ largest of two numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 > num2) {
+        printf("Largest number is: %d", num1);
+    }
+    else if (num2 > num1) {
+        printf("Largest number is: %d", num2);
+    }
+    else {
+        printf("Both numbers are equal.");
+    }
+
+    return 0;
+}
