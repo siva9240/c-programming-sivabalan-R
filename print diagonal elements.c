9580701d36f795114c1894be7935f2afa2da8a84
siva9240 <sivabalan9240@mountzion.ac.in>
2026-02-19 09:30:44 +0530

    Create print diagonal elements.c

---
+++ print diagonal elements.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);  
+
+    int matrix[n][n];
+
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", matrix[i][i]);
+    }
+
+    return 0;
+}
